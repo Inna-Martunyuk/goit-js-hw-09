f06980894186b04ff3207e0cc1fa22ddaed7561e
Inna-Martunyuk <imartunyuk88@gmail.com>
2024-12-26 19:16:02 +0200

    fix-hw-9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -6,44 +6,36 @@ let formData = {
 
 const fillFormFields = () => {
   try {
-    if (localStorage.length === 0) {
+    const formDataFromLS = localStorage.getItem('feedback-form-data');
+    if (!formDataFromLS) {
       return;
     }
-
-    const formDataFromLS = JSON.parse(
-      localStorage.getItem('feedback-form-data')
-    );
-
-    formData = formDataFromLS;
-
-    for (const key in formDataFromLS) {
-      feedbackFormEl.elements[key].value = formDataFromLS[key];
+    formData = JSON.parse(formDataFromLS);
+    for (const key in formData) {
+      if (feedbackFormEl.elements[key]) {
+        feedbackFormEl.elements[key].value = formData[key];
+      }
     }
   } catch (err) {
     console.log(err);
   }
 };
-
 fillFormFields();
 
 const onFormFieldInput = event => {
   const { target: formFieldEl } = event;
-
   const fieldValue = formFieldEl.value;
   const fieldName = formFieldEl.name;
-
   formData[fieldName] = fieldValue;
-
   localStorage.setItem('feedback-form-data', JSON.stringify(formData));
 };
 
 const onFeedbackFormSubmit = event => {
   event.preventDefault();
-
   const { currentTarget: formEl } = event;
-
   formEl.reset();
   localStorage.removeItem('feedback-form-data');
+  formData = { email: '', message: '' };
 };
 
 feedbackFormEl.addEventListener('input', onFormFieldInput);
